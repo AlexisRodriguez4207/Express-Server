aa91f3d83d3137b5fd1b00e5949a9cd6af4e74ec
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-04-29 00:12:17 -0700

    Agregando nuevas rutas y parametros para recibir

--- App.js
+++ App.js
@@ -18,3 +18,22 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx',(req,res) => {
+    res.send('Bienvenidos a LaunchX')
+})
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+  })
+
+//Query Params:Recibir parametros por la url
+// http://localhost:3000/explorer/Alexis
+// re.params = {"explorerName" ; "Alexis"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
